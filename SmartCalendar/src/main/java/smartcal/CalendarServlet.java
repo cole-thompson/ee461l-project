85b032919a40681be22dedd7e8f853ebb8e0161f
cole-thompson <colethompson@utexas.edu>
2017-11-17 21:10:48 -0600

    added calendar servlet, to handle event stuff

---
+++ SmartCalendar/src/main/java/smartcal/CalendarServlet.java
@@ -0,0 +1,30 @@
+package smartcal;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.googlecode.objectify.*;
+ 
+/*
+ * Servlet to handle stuff from the Calendar module
+ */
+
+public class CalendarServlet extends HttpServlet {
+	static {
+        //ObjectifyService.register();
+    }
+	
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+        
+        //String content = req.getParameter("content");
+        //ObjectifyService.ofy().save().entity().now(); 
+        
+        resp.sendRedirect("/ofyguestbook.jsp");
+    }
+}

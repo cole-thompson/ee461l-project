827efdd16c06bc01bd90d089ef92de1c3dff2b56
cole-thompson <colethompson@utexas.edu>
2017-11-17 21:07:55 -0600

    reverted file back to original
    we will probably just handle the login from the JSP file so I am going
    to just make a new servlet for the calendar form

--- SmartCalendar/src/main/java/smartcal/SmartCalendarLogin.java
+++ SmartCalendar/src/main/java/smartcal/SmartCalendarLogin.java
@@ -1,6 +1,5 @@
 package smartcal;
 
-
 import java.io.IOException;
 
 import javax.servlet.http.*;
@@ -10,42 +9,38 @@ import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 
 import com.google.appengine.api.users.UserServiceFactory;
-import com.googlecode.objectify.ObjectifyService;
 
  
 
 public class SmartCalendarLogin extends HttpServlet {
 
-	static {
-        //ObjectifyService.register();
-    }
-	
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 3275371858742144640L;
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 
-		UserService userService = UserServiceFactory.getUserService();
+              throws IOException {
+
+        UserService userService = UserServiceFactory.getUserService();
+
         User user = userService.getCurrentUser();
-        
-        //String content = req.getParameter("content");
-        //ObjectifyService.ofy().save().entity().now(); 
-        
-        resp.sendRedirect("/calendar.jsp");
+
+ 
+
+        if (user != null) {
+
+            resp.setContentType("text/plain");
+
+            resp.getWriter().println("Hello, " + user.getNickname());
+
+        } else {
+
+            resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+
+        }
 
     }
 
 }
-
-/*		//commented this out because it is currently in the jsp file
-        UserService userService = UserServiceFactory.getUserService();
-        User user = userService.getCurrentUser();
-        if (user != null) {
-            resp.setContentType("text/plain");
-            resp.getWriter().println("Hello, " + user.getNickname());
-        } else {
-            resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
-        }
-*/

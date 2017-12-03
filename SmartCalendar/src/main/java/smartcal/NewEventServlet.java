be46bea0abfc2164f6d085b5badcfb15f4647f49
LCED <ericc.li@utexas.edu>
2017-12-02 20:12:40 -0600

    edited NewEventServlet

--- SmartCalendar/src/main/java/smartcal/NewEventServlet.java
+++ SmartCalendar/src/main/java/smartcal/NewEventServlet.java
@@ -4,7 +4,10 @@ import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.googlecode.objectify.ObjectifyService;
+
+import java.util.List;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 
 import javax.servlet.http.HttpServlet;
@@ -27,8 +30,19 @@ public class NewEventServlet extends HttpServlet{
 		//get the current user
 		User user = UserServiceFactory.getUserService().getCurrentUser();
 		
+		//get FriendsList
+       	FriendsList flist = ObjectifyService.ofy().load().type(FriendsList.class).filter("user", user).first().now();
+       	if(flist == null){
+       		System.out.println(user + " didnt have a friendslist for some reason, creating it now");
+       		flist = new FriendsList(user);
+       		ObjectifyService.ofy().save().entity(flist);
+       	}else{
+       		System.out.println("friendslist found: \n" + flist);		// THIS STATEMENT IS TO DEBUG, PRINTS THE WHOLE FRIENDSLIST. CAN BE REMOVED
+       	}
+       	
        	//check all the different forms in newevent.jsp
-       	if (checkForm1(req, user)) {}
+		
+       	if (checkForm1(req, user, flist)) {}
        	else if (checkForm2(req, user)) {}
 		
 		//System.out.println("worked");
@@ -38,7 +52,7 @@ public class NewEventServlet extends HttpServlet{
 	 
 	//Stage1: Invitation creation
 	
-	private boolean checkForm1(HttpServletRequest req, User creator) {
+	private boolean checkForm1(HttpServletRequest req, User creator, FriendsList f) {
 		boolean pressed = false;
 		
 		if (req.getParameter("part1submit") != null) {
@@ -46,11 +60,23 @@ public class NewEventServlet extends HttpServlet{
 			String eventName = req.getParameter("eventname");
 			String eventType = req.getParameter("eventtype");
 			
+			List<User> eventFriends = new ArrayList<User>();
+			List<User> friends = f.getFriends();
+			
 			if(eventName == null || eventType == null) {
 				//indicate that you need to fill in the fields
 				System.out.println("One or more fields have not been filed in");
+				return true;
 			}
-			else if(eventType.equals("Generic")) {
+			
+			for(int i = 0;  i < friends.size(); i++) {
+				String name = "friend" + i;
+				if(req.getParameter(name) != null) {
+					eventFriends.add(f.getFriends().get(i));
+				}
+			}
+			
+			if(eventType.equals("Generic")) {
 				System.out.println("Event Type: Generic");
 				Invitation invite = new Invitation(creator);
 			}
@@ -61,6 +87,7 @@ public class NewEventServlet extends HttpServlet{
 			else {
 				System.out.println("Event Type: Invalid");
 			}
+			
     		pressed = true;
         }	
 		

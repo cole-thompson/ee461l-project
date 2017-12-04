0efe3e892b23efdd9d8bffa8894d10a01b132343
Christopher Sauceda <csauce@utexas.edu>
2017-12-03 23:15:08 -0600

    added functions for getting String information about the database
    getNumMovies, getNumTheatresByMovie, getShowtimesByMovieTheatre, getNumShowtimesByMovieTheatre, getTheatresByMovie, getMovieTitles

--- SmartCalendar/src/main/java/smartcal/Showtime.java
+++ SmartCalendar/src/main/java/smartcal/Showtime.java
@@ -9,9 +9,14 @@ public class Showtime {
 		
 		public String getDateTime() { return dateTime; }
 		
+		private String time;
+		
+		public String getTime() { return time; }
+		
 		public Showtime(String theater, String dateTime) {
 			this.theater = theater;
 			this.dateTime = dateTime;
+			time = dateTime.substring(dateTime.indexOf('T')+1);
 		}
 		
 		@Override

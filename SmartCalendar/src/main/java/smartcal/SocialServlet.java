573fa97a096bd7804c66fe48f870f7022c8d82ea
Matthew Tan <matthewtan@utexas.edu>
2017-12-03 20:55:16 -0600

    Debugging weird SocialServlet issue

--- SmartCalendar/src/main/java/smartcal/SocialServlet.java
+++ SmartCalendar/src/main/java/smartcal/SocialServlet.java
@@ -15,23 +15,28 @@ public class SocialServlet extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         //get the current user
     	User user = UserServiceFactory.getUserService().getCurrentUser();
+    	FriendsList currentUserFriends;
     	
     	if(req.getParameter("addfriend") != null){
-    		FriendsList currentUserFriends = ObjectifyService.ofy().load().type(FriendsList.class).filter("user", user).first().now();
+    		currentUserFriends = ObjectifyService.ofy().load().type(FriendsList.class).filter("user", user).first().now();
     		String toAdd = req.getParameter("friendname");
     		UserAccount friendAccount = ObjectifyService.ofy().load().type(UserAccount.class).filter("username", toAdd).first().now();
     		if(friendAccount == null) {
     			System.out.println("Friend doesn't have an account??????");
     		}
-    		currentUserFriends.addFriend(friendAccount.getUser());
+    		boolean worked = currentUserFriends.addFriend(friendAccount.getUser());
+    		ObjectifyService.ofy().save().entity(currentUserFriends);
+    		System.out.println(worked);
     	}else if(req.getParameter("removefriend") != null){
-    		FriendsList currentUserFriends = ObjectifyService.ofy().load().type(FriendsList.class).filter("user", user).first().now();
+    		currentUserFriends = ObjectifyService.ofy().load().type(FriendsList.class).filter("user", user).first().now();
     		String toAdd = req.getParameter("friendname");
     		UserAccount friendAccount = ObjectifyService.ofy().load().type(UserAccount.class).filter("username", toAdd).first().now();
     		if(friendAccount == null) {
     			System.out.println("Friend shouldn't have ever made it on your list since they have no list");
     		}
-    		currentUserFriends.removeFriend(friendAccount.getUser());
+    		boolean worked = currentUserFriends.removeFriend(friendAccount.getUser());
+    		ObjectifyService.ofy().save().entity(currentUserFriends);
+    		System.out.println(worked);
     	}
     	
         resp.sendRedirect("/friends.jsp");
